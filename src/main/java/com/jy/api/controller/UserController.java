0d34ec48efe5be9b29fe8929a411091bf7c7e61d
Justin <justinyang1000@hotmail.com>
2019-08-01 15:19:00 -0400

    Initial commit

---
+++ src/main/java/com/jy/api/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.jy.api.controller;
+
+import com.jy.api.entity.User;
+import com.jy.api.entity.request.AddUserRequest;
+import com.jy.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController(value = "users")
+public class UserController {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void addNewWebUser(@RequestBody AddUserRequest addUserRequest) {
+        User user = new User();
+        user.setName(addUserRequest.getName());
+        user.setSurname(addUserRequest.getSurname());
+        userRepository.save(user);
+    }
+
+}
